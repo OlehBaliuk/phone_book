cdf46d93394f9ca24e42f058e15a41161be40bd4
Oleh Baliuk <oleh.baliuk@nixsolutions.com>
2023-02-09 13:05:35 +0200

    init commit

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    trailingComma: 'all',
+    jsxBracketSameLine: false,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    endOfLine: 'auto',
+    singleAttributePerLine: false,
+};
